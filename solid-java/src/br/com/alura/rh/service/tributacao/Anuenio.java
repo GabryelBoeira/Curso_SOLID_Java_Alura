6a097ed25eb4a17e0e52d58671b9854d5c671c2c
GabryelBoeira <gabryel@asaplog.com.br>
2022-03-31 11:31:57 -0300

    Trabalhando conseito do SOLID do ISP: Interface Segregation Principle

---
+++ solid-java/src/br/com/alura/rh/service/tributacao/Anuenio.java
@@ -0,0 +1,30 @@
+package br.com.alura.rh.service.tributacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Anuenio implements Reajuste {
+
+    private BigDecimal valor;
+    private LocalDate data;
+
+    public Anuenio(BigDecimal valor, LocalDate data) {
+        this.valor = valor;
+        this.data = data;
+    }
+
+    @Override
+    public BigDecimal valor() {
+        return null;
+    }
+
+    @Override
+    public LocalDate data() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal valorImpostoDeRenda() {
+        return BigDecimal.ZERO;
+    }
+}
